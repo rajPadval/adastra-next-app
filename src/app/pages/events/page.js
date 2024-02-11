e01b1b880b01e15bd7312ed21e442be02695e551
rajPadval <97424854+rajPadval@users.noreply.github.com>
2024-02-11 23:08:11 +0530

    Modified event page

--- src/app/pages/events/page.js
+++ src/app/pages/events/page.js
@@ -17,11 +17,21 @@ const Events = () => {
   }, []);
 
   return (
-    <div className="text-white flex flex-col gap-16 justify-center items-center  my-28">
-      {events.map((event, i) => {
-        return <EventComp key={i} {...event} />;
-      })}
-    </div>
+    <section className="bg-gradient-to-b from-black to-transparent h-full">
+      <div className="pt-28 flex flex-col justify-center items-center ">
+        <h2 className="font-bebas text-7xl  bg-gradient-to-b  from-white to-gray-800 inline-block text-transparent bg-clip-text drop-shadow-xl mx-[5vw]">
+          EVENTS
+        </h2>
+        <p className="text-md lg:text-lg uppercase font-thin text-center mx-[5vw]">
+           Events that bridge the gap between learning and doing.
+        </p>
+      </div>
+      <div className="text-white flex flex-col gap-16 justify-center items-center  my-28">
+        {events.map((event, i) => {
+          return <EventComp key={i} {...event} />;
+        })}
+      </div>
+    </section>
   );
 };
 
