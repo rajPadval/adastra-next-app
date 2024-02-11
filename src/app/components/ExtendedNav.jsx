3aa28994eff4b5b535134ef37100a652f177837f
rajPadval <97424854+rajPadval@users.noreply.github.com>
2024-02-11 22:06:26 +0530

    ADded changes

--- src/app/components/ExtendedNav.jsx
+++ src/app/components/ExtendedNav.jsx
@@ -7,7 +7,7 @@ const ExtendedNav = ({ toggleNav, setToggleNav }) => {
     <div
       className={`${
         !toggleNav ? "translate-x-full" : "translate-x-0"
-      } fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-95 h-screen z-40 transition-all ease-linear duration-500 flex justify-center items-center`}
+      } fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-95 h-screen z-40 transition-all ease-in-out duration-500 flex justify-center items-center`}
     >
       <MdOutlineClose
         className="text-3xl text-white cursor-pointer absolute top-5 right-4 lg:hidden"
